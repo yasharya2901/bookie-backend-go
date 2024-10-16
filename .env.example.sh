7a9586af57a12ec59a0d2d19d764786895d0b990
Yash Arya <yasharya2901@gmail.com>
2024-10-17 00:25:39 +0530

    Added GIN_MODE environment variable in .env examples

--- .env.example.sh
+++ .env.example.sh
@@ -9,3 +9,4 @@ export DB_NAME="your_db_name"
 export DB_TIMEZONE="your_db_timezone"
 export APPWRITE_WEBHOOK_SECRET="your_secret_value"
 export PORT="8080" # Change this to your desired port
+export GIN_MODE="debug" # Change this to "release" for production
